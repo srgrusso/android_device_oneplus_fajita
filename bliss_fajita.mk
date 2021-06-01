62d13d110c8f37e2b11a805fec1c102e0a187f74
srgrusso <srgrusso240@gmail.com>
2021-05-31 22:11:35 -0500

    fajita: Set flag for FOD animations

--- bliss_fajita.mk
+++ bliss_fajita.mk
@@ -27,6 +27,9 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# FOD animations
+TARGET_WANTS_FOD_ANIMATIONS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_fajita
 PRODUCT_DEVICE := fajita
