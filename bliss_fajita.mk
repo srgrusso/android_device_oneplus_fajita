ce5e9fb1c8dfc381f778542d1ec9cc926b3ed8e7
srgrusso <srgrusso240@gmail.com>
2021-11-28 14:15:51 -0600

    Blissify

--- bliss_fajita.mk
+++ bliss_fajita.mk
@@ -21,14 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-# PixelExperience Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
-PRODUCT_NAME := aosp_fajita
+PRODUCT_NAME := bliss_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
