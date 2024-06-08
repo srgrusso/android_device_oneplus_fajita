8a7b789db509be4b84a0d5cae49eef3d2e78bba7
srgrusso <srgrusso240@gmail.com>
2024-06-08 15:06:35 -0500

    Blissify

--- bliss_fajita.mk
+++ bliss_fajita.mk
@@ -21,10 +21,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fajita
+# boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# udfps
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Gapps
+TARGET_ESSENTIAL_GAPPS := true
+
+PRODUCT_NAME := bliss_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
