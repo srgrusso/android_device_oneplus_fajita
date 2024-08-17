576b573bbe544240c8ee57c763ab38f28b22459f
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    fajita: Do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -44,8 +44,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
